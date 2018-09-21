b8315d68b6f135ad619727aa8dab2baf1613c11a
spl3en <spl3en.contact@gmail.com>
2018-09-22 00:02:16 +0200

    Update limit to 1000

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ from app.views import swap_status as swap_status_view
 @app.route('/')
 @app.route('/index')
 def index():
-    holders = top_holders_model.get_list ("0xb5a5f22694352c15b00323844ad545abb2b11028", 200)
+    holders = top_holders_model.get_list ("0xb5a5f22694352c15b00323844ad545abb2b11028", 1000)
     holders = top_holders_view.get_list (holders)
     return render_template ('index.html', constants=constants, holders=holders)
 
